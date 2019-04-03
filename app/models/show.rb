d262273104c3dbf175f9a6f87fada3388260003a
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-03 22:58:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where('rating = #{self.highest_rating}')
+    Show.where("rating = '#{self.highest_rating}'")
   end
+
+  def self.
 end
